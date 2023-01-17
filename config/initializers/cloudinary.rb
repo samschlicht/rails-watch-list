bb0e4e11d7925024ad5dbfc38a95189a7d830375
Sam Schlicht <samschlicht@gmail.com>
2023-01-18 00:04:59 +1100

    done

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dmjqqd9jn'
+  config.api_key = '224683327415472'
+  config.api_secret = 'JkZj9xpLMLtOX-_kVWg5a9eX1y0'
+  config.secure = true
+  config.cdn_subdomain = true
+end
